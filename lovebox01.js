8998c83de2aaa5a4abee4b25eaa07e8018c61cc1
Andreea <41521522+AndreeaCcss@users.noreply.github.com>
2019-02-20 19:31:53 +0200

    Add files via upload
    Axios version of HTTPS client

---
+++ lovebox01.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const result =  require('dotenv').config();
+
+if (result.error) { throw result.error };
+
+var text =  process.argv[2];
+
+axios.post('https://api-v3.loveboxlove.com/graphql', {
+  query: "mutation sendNewMessage($content: String!, $type: Int, $boxId: String!, $date: DateTime, $sendEmail: Boolean) { sendNewMessage(content: $content, boxId: $boxId, type: $type, date: $date, sendEmail: $sendEmail) { recipient } }",
+  variables: {
+    boxId: process.env.BOX_ID,
+    date: null,
+    content: `${text.padEnd(168)}`,
+    type: 1,
+    sendEmail: false
+    },
+  operationName: "sendNewMessage",
+}, {
+  headers: {Authorization : process.env.API_KEY,
+            "Content-Type": "application/json; charset=utf-8"
+  }
+})
+.then((res) => {
+  console.log(`statusCode: ${res.status}`)
+})
+.catch((error) => {
+  console.error(error)
+})
